deb405249adadb506fb50fdec7ec203f82e15bc6
Luka <luka.cetina@gmail.com>
2025-08-25 12:26:07 +0200

    cypress error

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: 'vrsv3n',
+  video: true,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
